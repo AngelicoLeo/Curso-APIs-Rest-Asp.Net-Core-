6690ea2ebe04328b3947f9ca2736a4c935f042a7
LeonardoAngelico <leoangelicox@gmail.com>
2020-01-15 12:02:25 -0300

    Aula Final

--- Alura.WebAPI.WebApp/Startup.cs
+++ Alura.WebAPI.WebApp/Startup.cs
@@ -1,15 +1,17 @@
-﻿using Alura.ListaLeitura.Persistencia;
-using Alura.ListaLeitura.Seguranca;
-using Alura.ListaLeitura.Modelos;
+﻿using Alura.ListaLeitura.Seguranca;
+using Alura.ListaLeitura.HttpClients;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Alura.WebAPI.WebApp.Formatters;
-using Microsoft.IdentityModel.Tokens;
 using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 
 namespace Alura.ListaLeitura.WebApp
 {
@@ -24,49 +26,26 @@ namespace Alura.ListaLeitura.WebApp
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<LeituraContext>(options => {
-                options.UseSqlServer(Configuration.GetConnectionString("ListaLeitura"));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services
+                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options  => {
+                    options.LoginPath = "/Usuario/Login";
             });
 
-            services.AddDbContext<AuthDbContext>(options => {
-                options.UseSqlServer(Configuration.GetConnectionString("AuthDB"));
+            services.AddHttpClient<LivroApiClient>(client => {
+                client.BaseAddress = new Uri(Configuration["ApiURIs:LivrosApi"]);
             });
 
-            services.AddIdentity<Usuario, IdentityRole>(options =>
+            services.AddHttpClient<AuthApiClient>(client =>
             {
-                options.Password.RequiredLength = 3;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireLowercase = false;
-            }).AddEntityFrameworkStores<AuthDbContext>();
-
-            services.ConfigureApplicationCookie(options => {
-                options.LoginPath = "/Usuario/Login";
+                client.BaseAddress = new Uri(Configuration["ApiURIs:AuthApi"]);
             });
-
-            services.AddTransient<IRepository<Livro>, RepositorioBaseEF<Livro>>();
 
             services.AddMvc(options => {
                 options.OutputFormatters.Add(new LivroCsvFormatter());
             }).AddXmlSerializerFormatters();
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = "JwtBearer";
-                options.DefaultChallengeScheme = "JwtBearer";
-            }).AddJwtBearer("JwtBearer", options => {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("alura-webapi-authentication-valid")),
-                    ClockSkew = TimeSpan.FromMinutes(5),
-                    ValidIssuer = "Alura.WebApp",
-                    ValidAudience = "Postman",
-                };
-            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
